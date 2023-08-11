93572e1a958d60a9c06fb06b137dd6c94a5cc96a
James Eapen <james.eapen@vai.org>
2023-08-11 11:36:20 -0400

    feat(reader): read to CSC sparse matrix form
    Uses a CpG and sample name index to store loci and sample info

--- R/reader.r
+++ R/reader.r
@@ -84,3 +84,37 @@ make_beta_mat <- function(encoded_matrix) {
   encoded_matrix[complete.cases(encoded_matrix)][
   , lapply(.SD, decode_beta), .SDcols = 4:ncol(encoded_matrix)][]
 }
+
+make_cpg_index <- function(cpg_bed_file) {
+  cpgs <- fread(cpg_bed_file, col.names = c("chr", "start"), drop = 3)
+  cpgs[, "cpg_id" := seq.int(nrow(cpgs))][]
+}
+
+#' Sparse Methylation matrix
+#'
+#' Make a methylation matrix with given scWGBS sample bed files
+#' @param cpg_bed_file The gzipped bed file with CpG loci. This is used to make the matrix index
+#' @param sample_list A list of sample names for the bed files
+#' @param sample_path Path to the directory containing sample bed files
+#'
+#' @importFrom data.table := setcolorder rbindlist
+#' @export
+#'
+#' @examples
+#' sample_list <- c("sample1", "sample2", "sample3")
+#' sample_matrix <- make_meth_mat(sample_list, "./pileup", "./data/cpgs.bed.gz")
+make_sparse_mat <- function(cpg_bed_file, sample_list, sample_path) {
+
+  cpg_index <- make_cpg_index(cpg_bed_file)
+
+  meth_mat_list <- vector("list", length(sample_list))
+  mapply(function(i) {
+      meth_mat_list[[i]] <<- read_sample(paste0(sample_path, sample_list[i], ".bed.gz"), TRUE)[
+        cpg_index, on = .(chr, start), .(cpg_id, encoded), nomatch = 0L
+      ][, "sample_id" := i]
+    }, seq_along(sample_list))
+
+  result <- rbindlist(meth_mat_list)
+  setcolorder(result, c("cpg_id", "sample_id", "encoded"))
+  list(cpg_index = cpg_index, sample_index = sample_list, sparse_mat = result)
+}
