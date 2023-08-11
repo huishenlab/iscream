a0ebba5d3029d609e23e350df9de38975e78b700
James Eapen <james.eapen@vai.org>
2023-08-11 12:15:46 -0400

    feat(reader): write the long form sparse matrix to MatrixMarket file

--- R/make_sparse_mat.R
+++ R/make_sparse_mat.R
@@ -1,17 +1,23 @@
 #' Sparse Methylation matrix
 #'
-#' Make a methylation matrix with given scWGBS sample bed files
+#' Make a sparse methylation matrix with given scWGBS sample bed files. The
+#' matrix is sparse in that it stores the cpg locus id, the sample id and the
+#' associated endcoded beta and coverage value. It is not a true sparse matrix
+#' as you would find in the Matrix package and has no functions associated with
+#' it.
 #' @param cpg_bed_file The gzipped bed file with CpG loci. This is used to make the matrix index
 #' @param sample_list A list of sample names for the bed files
 #' @param sample_path Path to the directory containing sample bed files
+#' @param to_mm Whether to write the matrix to a matrix market file
+#' @param file_ext The file extension of the sample files
 #'
-#' @importFrom data.table := setcolorder rbindlist
+#' @importFrom data.table := setcolorder rbindlist fwrite
 #' @export
 #'
 #' @examples
 #' sample_list <- c("sample1", "sample2", "sample3")
-#' sample_matrix <- make_meth_mat(sample_list, "./pileup", "./data/cpgs.bed.gz")
-make_sparse_mat <- function(cpg_bed_file, sample_list, sample_path, file_ext = ".bed.gz") {
+#' sparse_mat <- make_sparse_mat(sample_list, "./pileup", "./data/cpgs.bed.gz")
+make_sparse_mat <- function(cpg_bed_file, sample_list, sample_path, to_mm = TRUE, file_ext = ".bed.gz") {
 
   cpg_index <- make_cpg_index(cpg_bed_file)
 
@@ -25,6 +31,24 @@ make_sparse_mat <- function(cpg_bed_file, sample_list, sample_path, file_ext = "
   result <- rbindlist(meth_mat_list)
   setcolorder(result, c("cpg_id", "sample_id", "encoded"))
   list(cpg_index = cpg_index, sample_index = sample_list, sparse_mat = result)
+
+  # TODO: option if chunking is necessary
+
+  if (to_mm) {
+    message("Writing cpg_index")
+    fwrite(cpg_index, "cpg_index.gz", sep = "\t", compress = "gzip")
+    message("Writing sample_list")
+    write(sample_list, "sample_list.txt")
+    message("Writing methylation matrix")
+    fwrite(list("%%MatrixMarket matrix coordinate real general"), "result.mtx", compress = "gzip")
+    fwrite(
+      list(nrow(cpg_index), length(sample_list), nrow(result)),
+       "result.mtx", sep = " ", compress = "gzip", append = TRUE)
+    fwrite(result, "result.mtx", sep = " ", append = TRUE, compress = "gzip")
+    message("Done")
+  } else {
+    list(cpg_index = cpg_index, sample_index = sample_list, sparse_mat = result)
+  }
 }
 
 make_cpg_index <- function(cpg_bed_file) {
