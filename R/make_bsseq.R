e395f9ed3592ed9ccd80e564f2911c5079d8ac0c
James Eapen <james.eapen@vai.org>
2023-08-11 12:46:52 -0400

    feat(reader): low memory bsseq creator from sparse

--- R/make_bsseq.R
+++ R/make_bsseq.R
@@ -1,6 +1,8 @@
 #' Make a BSSeq object
 #'
-#' Create a BSSeq object using a encoded sparse methylation matrix
+#' Create a BSSeq object using a encoded sparse methylation matrix. This method
+#' uses more memory than `make_bsseq_lm` since it uses dense coverage and
+#' methylation matrices when creating the BSSeq object.
 #' @param encoded_matrix A sparse matrix of CpG loci and samples with encoded beta values
 #' @importFrom data.table dcast setnafill
 #'
@@ -8,7 +10,7 @@
 #'
 #' @export
 #'
-make_bsseq_from_sparse <- function(sp, sample_list) {
+make_bsseq_hm <- function(sp, sample_list) {
 
   message("Making Cov matrix")
   C <- DelayedArray::DelayedArray(
@@ -43,6 +45,51 @@ make_bsseq_from_sparse <- function(sp, sample_list) {
     gr = gr,
     M = M,
     Cov = C,
+    sampleNames = sample_list
+  )
+}
+
+#' Make a BSSeq object using less memory
+#'
+#' Create a BSSeq object using a encoded sparse methylation matrix. This method
+#' uses less memory than `make_bsseq_hm` since it uses sparse coverage and
+#' methylation matrices when creating the BSseq object. It is however slower in
+#' the BSseq object creation.
+#' @param encoded_matrix A sparse matrix of CpG loci and samples with encoded beta values
+#' @importFrom data.table dcast setnafill
+#' @importFrom Matrix readMM
+#'
+#' @return A BSSeq object
+#'
+#' @export
+#'
+make_bsseq_lm <- function(
+    sparse_mat,
+    sample_list,
+    cov_filename = "cov.mtx.gz",
+    m_filename = "M.mtx.gz"
+) {
+  row_count <- nrow(sparse_mat$cpg_index)
+  col_count <- length(sample_list)
+
+  message("Writing coverage matrix market file")
+  mm_writer(sparse_mat$sparse_mat[, c(1, 2, 3)], row_count, col_count, cov_filename)
+
+  message("Writing M matrix market file")
+  mm_writer(sparse_mat$sparse_mat[, c(1, 2, 4)], row_count, col_count, m_filename)
+
+  message("Making GRanges")
+  gr <- GRanges(sparse_mat$cpg_index[, end := start + 2][, cpg_id := NULL])
+  gc()
+
+  cov_mat <- DelayedArray(readMM(cov_filename))
+  M_mat <- DelayedArray(readMM(m_filename))
+
+  message("Making BSseq object")
+  BSseq(
+    gr = gr,
+    Cov = cov_mat,
+    M = M_mat,
     sampleNames = sample_list
   )
 }
