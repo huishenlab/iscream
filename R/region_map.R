45e09a4e70ffb55d65119939ab43c2f43f9a8163
James Eapen <james.eapen@vai.org>
2024-05-14 14:57:51 -0400

    feat(region_map): add R wrapper for region_map with error checking

---
+++ R/region_map.R
@@ -0,0 +1,39 @@
+#' Run a defined function over genomic regions
+#'
+#' Run a function on the CpGs in bedfiles across genomic regions. Currently
+#' supported functions are aggregate, average, and ...
+#' @param befiles A vector of bedfile paths
+#' @param regions A vector of genomic regions strings
+#' @param fun Function to apply over the region. See details.
+#' @param mval Whether to calculate the m value (coverage \eqn{$\times$ beta})
+#' when applying the function.
+#' @details
+#' Available function:
+#' - "aggregate"` sums the values in the region with aggregated beta values if
+#' `mval =` FALSE and aggregated M values if `mval =` TRUE
+#' - "average"` averages the values in the region with average beta values if
+#' `mval =` FALSE and average M values if `mval =` TRUE
+#' @importFrom fs file_exists
+#' @return A data.frame
+#'
+#' @export
+region_map <- function(bedfiles, regions, fun = "aggregate", mval = TRUE) {
+
+  supported_funcs <- c("aggregate", "average")
+  stopifnot("Selected function not supported" = fun %in% supported_funcs)
+
+  valid_files <- sapply(bedfiles, fs::file_exists)
+  missing_files <- bedfiles[!valid_files]
+  if (length(missing_files != 0)) {
+    stop(paste0("File: ", missing_files, " could not be found"))
+  }
+
+  valid_regions <- sapply(regions, function(i) grepl("^chr", i))
+  invalid_regions <- regions[!valid_regions]
+  if (length(invalid_regions != 0)) {
+    stop(paste0(invalid_regions, " are invalid regions"))
+  }
+
+  n_threads <- getOption("iscream.threads")
+  cpg_apply(bedfiles, regions, fun, mval, nthreads = n_threads)
+}
