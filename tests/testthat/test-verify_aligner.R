51de36e0af551379d49b5fed47d815f4145cecf5
James Eapen <james.eapen@vai.org>
2024-10-05 14:18:57 -0400

    test(verify_filetype): add tests for aligner/filetype check

---
+++ tests/testthat/test-verify_aligner.R
@@ -0,0 +1,41 @@
+bismark1 <- paste0(letters[1:10], ".cov.gz")
+bismark2 <- paste0(letters[1:10], ".cov")
+biscuit1 <- paste0(letters[1:10], ".bed.gz")
+biscuit2 <- paste0(letters[1:10], ".bed")
+biscuit3 <- paste0(letters[1:10], ".mergecg.bed.gz")
+bismark <- c(bismark1, bismark2)
+biscuit <- c(biscuit1, biscuit2, biscuit3)
+
+test_that("verify_aligner_or_stop valid bismark", {
+  expect_no_error(iscream:::verify_filetype(bismark, "bismark"))
+})
+
+test_that("verify_aligner_or_stop valid biscuit", {
+  expect_no_error(iscream:::verify_filetype(bismark, "bsbolt"))
+})
+
+test_that("verify_aligner_or_stop valid", {
+  expect_no_error(iscream:::verify_filetype(biscuit, "biscuit"))
+})
+
+test_that("verify_aligner_or_stop valid", {
+  expect_warning(
+    iscream:::verify_filetype(biscuit, "bismark"),
+    "'aligner' set to bismark but no files found with '.cov', extension - verify aligner and data frame colnames"
+  )
+})
+
+
+test_that("verify_aligner_or_stop valid", {
+  expect_warning(
+    iscream:::verify_filetype(biscuit, "bsbolt"),
+    "'aligner' set to bsbolt but no files found with '.cov', extension - verify aligner and data frame colnames"
+  )
+})
+
+test_that("verify_aligner_or_stop valid", {
+  expect_warning(
+    iscream:::verify_filetype(bismark, "biscuit"),
+    "'aligner' set to 'biscuit' but files found with '.cov', extension - verify aligner and data frame colnames"
+  )
+})
