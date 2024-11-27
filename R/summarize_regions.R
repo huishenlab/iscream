565f53657a45e4a3593b0a5c5a0978b794313d70
James Eapen <james.eapen@vai.org>
2024-11-26 19:05:16 -0500

    feat(summarize_regions): add summarize_to_sample for sample level stats

--- R/summarize_regions.R
+++ R/summarize_regions.R
@@ -116,6 +116,89 @@ summarize_regions <- function(
   colnames(df)[which(colnames(df) == mval_cpg_count)] <- "cpg_count"
   df
 }
+
+#' Summarize CpGs methylation information over cells
+#'
+#' Get average methylation information for the full sample
+#' @param chr_summary A `summarize_regions()` table with chromosomes as regions
+#'
+#' @details
+#' From https://stats.stackexchange.com/a/389925
+#'
+#' \eqn{\mu = \frac{\sum_{n=1}^{n} \mu_{\text{chr}} * n_{\text{chr}}}{\sum_{n=1}^{n} n_{\text{chr}}}}
+#'
+#' \eqn{\text{Total CpGs = }N = \sum\limits_{c=1}^{C} n}
+#'
+#' \eqn{\text{Sample Mean = }\mu = \frac{\sum\limits_{c=1}^{C} \mu_{\text{c}} n}{N}}
+#'
+#' \eqn{\text{Sample Variance} = \sigma^2 = \frac{\sum\limits_{\text{c}=1}^C n(\mu_\text{c}-\mu)^2+\sum\limits_{\text{c}=1}^C n \sigma^2_\text{c}}{N}}
+#'
+#' \eqn{\text{Sample stddev} = \sigma = \sqrt{\sigma^2}}
+#'
+#' @importFrom data.table setDT
+#'
+#' @return A data.table
+#'
+#' @export
+#'
+#' @examples
+#' bedfiles <- system.file("extdata", package = "iscream") |>
+#'   list.files(pattern = "[a|b|c|d].bed.gz$", full.names = TRUE)
+#'
+#' chr_summary <- summarize_regions(bedfiles, query_chroms(bedfiles))
+#' summarize_to_sample(chr_summary)
+summarize_to_sample <- function(chr_summary) {
+
+  `.` <- list()
+  Sample <- NULL
+  cpg_count <- total_cpg_count <- NULL
+  beta.mean <- coverage.mean <- NULL
+  beta.min <- coverage.min <- NULL
+  beta.max <- coverage.max <- NULL
+  beta.range <- coverage.range <- NULL
+  beta.variance <- coverage.variance <- NULL
+  total_beta.mean <- total_coverage.mean <- NULL
+  total_beta.min <- total_coverage.min <- NULL
+  total_beta.max <- total_coverage.max <- NULL
+  total_beta.range <- total_coverage.range <- NULL
+  total_beta.variance <- total_coverage.variance <- NULL
+  total_beta.stddev <- total_coverage.stddev <- NULL
+
+  summary.dt <- setDT(chr_summary)
+
+  unique(
+    summary.dt
+    [, total_cpg_count := sum(cpg_count, na.rm = TRUE), by = Sample]
+    [, total_beta.mean := sum(beta.mean * cpg_count, na.rm = TRUE) / total_cpg_count, by = Sample]
+    [, total_coverage.mean := sum(coverage.mean * cpg_count, na.rm = TRUE) / total_cpg_count, by = Sample]
+    [, total_beta.min := min(beta.min, na.rm = TRUE), by = Sample]
+    [, total_coverage.min := min(coverage.min, na.rm = TRUE), by = Sample]
+    [, total_beta.max := max(beta.max, na.rm = TRUE), by = Sample]
+    [, total_coverage.max := max(coverage.max, na.rm = TRUE), by = Sample]
+    [, total_beta.range := max(total_beta.max, na.rm = TRUE) - min(total_beta.min, na.rm = TRUE), by = Sample]
+    [, total_coverage.range := max(total_coverage.max, na.rm = TRUE) - min(total_coverage.min, na.rm = TRUE), by = Sample]
+    [, total_beta.variance := (sum(cpg_count * (beta.mean - total_beta.mean)^2, na.rm = TRUE) + sum(cpg_count * beta.variance, na.rm = TRUE)) / total_cpg_count, by = Sample]
+    [, total_coverage.variance := (sum(cpg_count * (coverage.mean - total_coverage.mean)^2, na.rm = TRUE) + sum(cpg_count * coverage.variance, na.rm = TRUE)) / total_cpg_count, by = Sample]
+    [, total_beta.stddev := sqrt(total_beta.variance), by = Sample]
+    [, total_coverage.stddev := sqrt(total_coverage.variance), by = Sample]
+    [, .(
+      Sample,
+      beta.mean = total_beta.mean,
+      coverage.mean = total_coverage.mean,
+      beta.min = total_beta.min,
+      coverage.min = total_coverage.min,
+      beta.max = total_beta.max,
+      coverage.max = total_coverage.max,
+      beta.range = total_beta.range,
+      coverage.range = total_coverage.range,
+      beta.variance = total_beta.variance,
+      coverage.variance = total_coverage.variance,
+      beta.stddev = total_beta.stddev,
+      coverage.stddev = total_coverage.stddev
+    )]
+  )
+}
+
 #' Read WGBS BED file and get summary statistics
 #'
 #' @param bedfile Path to the BED file
