e56427b2291597686b995fa9908149db7e82b838
James Eapen <james.eapen@vai.org>
2024-11-29 15:33:30 -0500

    test(utils): test uninstalled GenomicRanges for get_granges_string

--- tests/testthat/test-get_regions.R
+++ tests/testthat/test-get_regions.R
@@ -1,7 +1,17 @@
 options("iscream.threads" = 1)
-
 regions <- c("chr1:1-6", "chr1:7-10", "chr1:11-14")
 gr <- GenomicRanges::GRanges(regions)
+
+with_mocked_bindings(
+    {
+    expect_error(
+      get_granges_string(gr),
+      "The 'GenomicRanges' package must be installed for this functionality"
+    )
+  },
+  requireNamespace = function(package, ..., quietly=FALSE) FALSE,
+  .package="base"
+)
 
 test_that("Test GRanges to string", {
   expect_equal(get_granges_string(gr), regions)
