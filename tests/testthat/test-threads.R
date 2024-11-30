a26da32d056c5d24da578b00f63c9fe770f53a35
James Eapen <james.eapen@vai.org>
2024-11-30 11:55:21 -0500

    test(threads): when options are not set

--- tests/testthat/test-threads.R
+++ tests/testthat/test-threads.R
@@ -11,9 +11,20 @@ test_that("set_threads within limit", {
 
 test_that("set_threads over limit", {
   expect_error(set_threads(999))
-  expect_error(set_threads(parallelly::availableCores() + 1))
+  expect_error(set_threads(availableCores() + 1))
 })
 
 test_that("set threads option over limit", {
   expect_error(check_thread_count(999, opt_set = TRUE))
+})
+
+options("iscream.threads" = NULL)
+expected_threads <- c(
+  'use_threads' = 1,
+  opt_set = FALSE,
+  avail_threads = unname(availableCores())
+)
+
+test_that("get threads with NULL option", {
+  expect_equal(get_threads(), expected_threads)
 })
