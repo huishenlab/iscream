bd489a43e495966dd70461808cb0b42caad707d1
James Eapen <james.eapen@vai.org>
2025-02-11 09:38:00 -0500

    test(query_all): test for beta matrix

--- tests/testthat/test-query_all.R
+++ tests/testthat/test-query_all.R
@@ -26,14 +26,31 @@ Cov <- matrix(c(1, 2, 0, 1,
                  3, 1, 0, 1),
                nrow = 7,
                byrow = TRUE)
+beta <- matrix(
+  c(1, 0, 0, 1,
+    1, 0, 1, 1,
+    0, 1, 0, 0,
+    0, 1, 0, 0,
+    0.5, 0, 1, 0.5,
+    1, 0, 0, 0,
+    1, 1, 0, 1),
+  nrow = 7,
+  byrow = TRUE
+)
+
+
 M_sp <- Matrix(M, sparse = T)
 Cov_sp <- Matrix(Cov, sparse = T)
+beta_sp <- Matrix(beta, sparse = T)
+
 row_names <- c("chr1:2", "chr1:4", "chr1:6", "chr1:8", "chr1:10", "chr1:12", "chr1:14")
 col_names <- c("a", "b", "c", "d")
 colnames(M) <- col_names
 colnames(Cov) <- col_names
+colnames(beta) <- col_names
 colnames(M_sp) <- col_names
 colnames(Cov_sp) <- col_names
+colnames(beta_sp) <- col_names
 
 exp_pos <- seq(1, 13, by = 2)
 exp_seq <- rep("chr1", 7)
@@ -45,13 +62,20 @@ biscuit_sparse_test <- query_all(biscuit_bedfiles, regions, sparse = T)
 bismark_sparse_test <- query_all(bismark_bedfiles, regions, sparse = T, aligner = "bismark")
 biscuit_granges_test <- query_all(biscuit_bedfiles, gr, prealloc = 2)
 biscuit_df_test <- query_all(biscuit_bedfiles, regions.dt, prealloc = 2)
+biscuit_beta_test <- query_all(biscuit_bedfiles, regions, sparse = F, mval = F)
+bismark_beta_test <- query_all(bismark_bedfiles, regions, sparse = F, aligner = "bismark", mval = F)
+biscuit_sparse_beta_test <- query_all(biscuit_bedfiles, regions, sparse = T, mval = F)
+bismark_sparse_beta_test <- query_all(bismark_bedfiles, regions, sparse = T, aligner = "bismark", mval = F)
+
 results <- list(
   biscuit_test,
   bismark_test,
   biscuit_sparse_test,
   bismark_sparse_test,
   biscuit_granges_test,
-  biscuit_df_test
+  biscuit_df_test,
+  biscuit_sparse_beta_test,
+  bismark_sparse_beta_test
 )
 
 dense_class <- "matrix"
@@ -80,32 +104,54 @@ test_that("matrix metadata", {
   test_types(biscuit_df_test, sparse = F)
 })
 
-test_content <- function(result_obj, sparse) {
+test_content <- function(result_obj, sparse, mval) {
 
   if (sparse) {
-    exp_m <- M_sp
+    exp_m <- if (mval) {
+      M_sp
+    } else {
+      beta_sp
+    }
     exp_c <- Cov_sp
   } else {
-    exp_m <- M
+    exp_m <- if (mval) {
+      M
+    } else {
+      beta
+    }
     exp_c <- Cov
   }
 
-  expect_equal(result_obj$M, exp_m)
+
+  m_beta <- ifelse(mval, result_obj$M, result_obj$beta)
+  if (mval) {
+    expect_equal(result_obj$M, exp_m)
+  } else {
+    expect_equal(result_obj$beta, exp_m)
+  }
   expect_equal(result_obj$Cov, exp_c)
 
 }
+
 test_that("matrix content", {
-  lapply(results[1:2], test_content, FALSE)
-  lapply(results[5], test_content, FALSE)
-  lapply(results[3:4], test_content, TRUE)
+  lapply(results[1:2], test_content, FALSE, TRUE)
+  lapply(results[5], test_content, FALSE, TRUE)
+  lapply(results[3:4], test_content, TRUE, TRUE)
+  lapply(results[7:8], test_content, TRUE, FALSE)
 })
 
 exp_dims <- c(7, length(biscuit_bedfiles))
-test_dims <- function(result_obj, exp_dims) {
-  expect_equal(dim(result_obj$M), exp_dims)
+test_dims <- function(result_obj, exp_dims, mval) {
+  if ("beta" %in% names(result_obj)) {
+    mbeta_name <- "beta"
+  } else {
+    mbeta_name <- "M"
+  }
+  expect_equal(dim(result_obj[[mbeta_name]]), exp_dims)
   expect_equal(dim(result_obj$Cov), exp_dims)
 }
 
 test_that("matrix row,colnames,sampleNames", {
-  lapply(results, test_dims, exp_dims)
+  lapply(results[1], test_dims, exp_dims)
 })
+
