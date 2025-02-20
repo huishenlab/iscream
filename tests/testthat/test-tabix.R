75d2b82457bb99c91d825d86cbe534ee24c10552
James Eapen <james.eapen@vai.org>
2025-02-20 14:48:38 -0500

    test(tabix): add htslib v shell tabix tests, reorganize

--- tests/testthat/test-tabix.R
+++ tests/testthat/test-tabix.R
@@ -1,63 +1,88 @@
 library(data.table)
+
+# input data
 extdata <- system.file("extdata", package = "iscream")
 chrom_beds <- list.files(paste0(extdata, "/test_chroms"), pattern = "*.bed.gz$", full.names = T)
 biscuit_tabix_beds <- list.files(extdata, pattern = "[a|b|c|d].bed.gz$", full.names = T)
 bismark_tabix_beds <- list.files(extdata, pattern = "[a|b|c|d].cov.gz$", full.names = T)
-tabix_df_result <- list.files(extdata, pattern = "tabix_dataframe.test", full.names = T)
-tabix_df_result_bismark <- list.files(extdata, pattern = "tabix_dataframe_bismark.test", full.names = T)
 mergecg_bed <- list.files(extdata, pattern = "*_mergecg.bed.gz$", full.names = T)
+
 regions <- c(A = "chr1:1-6", B = "chr1:7-10", C = "chr1:11-14")
 regions.dt <- as.data.table(regions)[, tstrsplit(regions, ":|-")]
 colnames(regions.dt) <- c("chr", "start", "end")
+
 gr <- GenomicRanges::GRanges(regions)
 gr.meta <- GenomicRanges::GRanges(regions)
 GenomicRanges::values(gr.meta) <- data.frame(meta = c("s1", "s2", "s3"))
 
-tabix_raw_res <- list(
-    `chr1:1-6` =  c("chr1\t3\t4\t1.000\t2"),
-    `chr1:7-10` = c("chr1\t7\t8\t0.000\t2", "chr1\t9\t10\t1.000\t1"),
-    `chr1:11-14` = character(0)
-)
+# testing data
+tabix_df_result <- list.files(extdata, pattern = "tabix_dataframe.test", full.names = T)
+tabix_df_result_bismark <- list.files(extdata, pattern = "tabix_dataframe_bismark.test", full.names = T)
 
-test_that("tabix dataframe", {
-  expect_equal(
-    tabix(biscuit_tabix_beds[1], regions),
-    fread(tabix_df_result, colClasses = c("character", "numeric", "numeric", "numeric", "numeric"))
-  )
-  expect_equal(
-    tabix(biscuit_tabix_beds[1], gr),
-    fread(tabix_df_result, colClasses = c("character", "numeric", "numeric", "numeric", "numeric")) |> GenomicRanges::GRanges()
-  )
-  expect_equal(
-    tabix(biscuit_tabix_beds[1], gr.meta),
-    fread(tabix_df_result, colClasses = c("character", "numeric", "numeric", "numeric", "numeric"))[,
-      meta := c(rep("s1", 3), rep("s2", 2), rep("s3", 2))
-    ] |> GenomicRanges::GRanges()
-  )
-  expect_equal(
-    tabix(biscuit_tabix_beds[1], regions.dt),
-    fread(tabix_df_result, colClasses = c("character", "numeric", "numeric", "numeric", "numeric"))
-  )
-  expect_equal(
-    tabix(bismark_tabix_beds[1], regions, aligner = 'bismark'),
-    fread(tabix_df_result_bismark, colClasses = c("character", "numeric", "numeric", "numeric", "numeric", "numeric"))
-  )
-})
+# test dataframe output
+test_tabix_dataframe <- function(htslib = FALSE) {
+  if (htslib) {
+    options("tabix.method" = "htslib")
+  }
+  test_that("tabix dataframe with", {
+    expect_equal(
+      tabix(biscuit_tabix_beds[1], regions),
+      fread(tabix_df_result, colClasses = c("character", "numeric", "numeric", "numeric", "numeric"))
+    )
+    expect_equal(
+      tabix(biscuit_tabix_beds[1], gr),
+      fread(tabix_df_result, colClasses = c("character", "numeric", "numeric", "numeric", "numeric")) |> GenomicRanges::GRanges()
+    )
+    expect_equal(
+      tabix(biscuit_tabix_beds[1], gr.meta),
+      fread(tabix_df_result, colClasses = c("character", "numeric", "numeric", "numeric", "numeric"))[,
+        meta := c(rep("s1", 3), rep("s2", 2), rep("s3", 2))
+        ] |> GenomicRanges::GRanges()
+    )
+    expect_equal(
+      tabix(biscuit_tabix_beds[1], regions.dt),
+      fread(tabix_df_result, colClasses = c("character", "numeric", "numeric", "numeric", "numeric"))
+    )
+    expect_equal(
+      tabix(bismark_tabix_beds[1], regions, aligner = 'bismark'),
+      fread(tabix_df_result_bismark, colClasses = c("character", "numeric", "numeric", "numeric", "numeric", "numeric"))
+    )
+    # empty
+    expect_warning(
+      tabix(chrom_beds[2], regions),
+      "No records found"
+    )
+  })
+  options("tabix.method" = "shell")
+}
 
-test_that("tabix raw list", {
-  expect_equal(
-    tabix(biscuit_tabix_beds[3], regions, raw = T),
-    tabix_raw_res
-  )
-})
+test_tabix_dataframe(htslib = FALSE)
+test_tabix_dataframe(htslib = TRUE)
 
-test_that("tabix dataframe", {
-  expect_warning(
-    tabix(chrom_beds[2], regions),
-    "No records found"
-  )
-})
+test_multi_tabix_dataframe <- function(htslib = FALSE) {
+  if (htslib) {
+    options("tabix.method" = "htslib")
+  }
+  multi_tabix <- lapply(biscuit_tabix_beds, function(i) {
+    tabix(i, regions)[,
+    sample := tools::file_path_sans_ext(basename(i), compression = TRUE)
+    ]
+  }) |> rbindlist()
 
+  test_that("tabix multi query", {
+    expect_equal(
+      tabix(biscuit_tabix_beds, regions),
+      multi_tabix
+    )
+  })
+  options("tabix.method" = "shell")
+}
+
+test_multi_tabix_dataframe(htslib = FALSE)
+test_multi_tabix_dataframe(htslib = TRUE)
+
+
+# test colnames
 custom_cols <- paste0("c", 1:5)
 test_that("tabix custom colnames", {
   expect_equal(
@@ -93,19 +118,20 @@ test_that("tabix mergecg colnames", {
   )
 })
 
+# test raw output
+tabix_raw_res <- list(
+    `chr1:1-6` =  c("chr1\t3\t4\t1.000\t2"),
+    `chr1:7-10` = c("chr1\t7\t8\t0.000\t2", "chr1\t9\t10\t1.000\t1"),
+    `chr1:11-14` = character(0)
+)
 
-multi_tabix <- lapply(biscuit_tabix_beds, function(i) {
-  tabix(i, regions)[,
-  sample := tools::file_path_sans_ext(basename(i), compression = TRUE)
-  ]
-}) |> rbindlist()
-
-test_that("tabix multi query", {
+test_that("tabix raw list", {
   expect_equal(
-    tabix(biscuit_tabix_beds, regions),
-    multi_tabix
+    tabix(biscuit_tabix_beds[3], regions, raw = T),
+    tabix_raw_res
   )
 })
+
 
 multi_tabix_raw <- lapply(biscuit_tabix_beds, function(i) {
   tabix(i, regions, raw = T)
