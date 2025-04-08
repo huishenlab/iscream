a501baa86a59bf4a1458b8064bed90ac2922b520
James Eapen <james.eapen@vai.org>
2025-04-08 14:35:54 -0400

    fix(summarize_regions): update with BedRecord

--- src/summarize_regions.cpp
+++ src/summarize_regions.cpp
@@ -70,9 +70,9 @@ DataVec make_data_vec(const std::vector<std::string>& cpgs, const bool mval, con
     arma::vec mvals(cpgs.size());
 
     for (int i = 0; i < cpgs.size(); i++) {
-        BedLine parsed_cpg = bismark ? parseCovRecord(cpgs[i]) : parseBiscuitRecord(cpgs[i]);
-        covs[i] = parsed_cpg.cov;
-        mvals[i] = mval ? parsed_cpg.m_count : parsed_cpg.beta;
+        BedRecord parsed_cpg = bismark ? parseCovRecord(cpgs[i]) : parseBiscuitRecord(cpgs[i]);
+        covs[i] = parsed_cpg.data[1];
+        mvals[i] = mval ? parsed_cpg.data[2] : parsed_cpg.data[0];
     }
     return DataVec{covs, mvals};
 }
