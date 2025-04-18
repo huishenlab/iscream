0a88e0e95244c1e4a423b9cf8e2e5c3dcf709a0e
James Eapen <james.eapen@vai.org>
2025-04-18 14:37:39 -0400

    test(summarize_regions): test for summarize_regions

--- tests/testthat/test-summarize_regions.R
+++ tests/testthat/test-summarize_regions.R
@@ -11,16 +11,15 @@ colnames(regions.dt) <- c("chr", "start", "end", "names")
 gr <- GenomicRanges::GRanges(regions)
 supported_funcs <- c("sum", "mean", "median", "stddev", "variance", "min", "max", "range")
 
-# utils
-get_colnames <- function(mval, funcs) {
+get_colnames <- function(funcs, col_names) {
   base_colnames <- c("Feature", "Sample")
-  values <- c("coverage", ifelse(mval, "M", "beta"))
+  values <- c(col_names)
   if ("all" %in% funcs) {
-    return(c(base_colnames, as.vector(outer(values, supported_funcs, paste, sep = ".")), "cpg_count"))
+    return(c(base_colnames, as.vector(outer(values, supported_funcs, paste, sep = ".")), "count"))
   }
 
-  if ("cpg_count" %in% funcs) {
-    index <- which(funcs == "cpg_count")
+  if ("count" %in% funcs) {
+    index <- which(funcs == "count")
     funcs <- funcs[-index]
   }
   function_used <- funcs
@@ -29,19 +28,25 @@ get_colnames <- function(mval, funcs) {
     as.vector(outer(values, function_used, paste, sep = "."))
   )
 
-  if ("cpg_count" %in% funcs) {
-  col_names <- c(col_names, "cpg_count")
+  if ("count" %in% funcs) {
+  col_names <- c(col_names, "count")
   }
 
   return(col_names)
 }
 
-run_test <- function(bedfiles, regions, funcs, mval, nthreads) {
-  colnames <- get_colnames(mval, funcs)
+run_test <- function(bedfiles, regions, funcs, columns, col_names, nthreads) {
+  test_colnames <- get_colnames(funcs, col_names)
+  reg_length <- if ("data.frame" %in% class(regions)) {
+    nrow(regions)
+  } else {
+      length(regions)
+  }
+
   list(
-    df = summarize_meth_regions(bedfiles, regions, fun = funcs, mval = mval, nthreads = nthreads),
-    colnames = colnames,
-    dims = c(length(bedfiles) * length(regions), length(colnames))
+    df = summarize_regions(bedfiles, regions, fun = funcs, columns = columns, col_names = col_names, nthreads = nthreads),
+    colnames = test_colnames,
+    dims = c(length(bedfiles) * reg_length, length(test_colnames))
   )
 }
 
@@ -50,7 +55,8 @@ one_file <- lapply(biscuit_bedfiles, function(i) {
     i,
     regions,
     funcs = "all",
-    mval = FALSE,
+    c(4, 5),
+    c("beta", "coverage"),
     nthreads = 1
   )
 })
@@ -60,7 +66,8 @@ one_fun <- lapply(supported_funcs, function(i) {
     biscuit_bedfiles,
     regions,
     funcs = i,
-    mval = FALSE,
+    c(4, 5),
+    c("beta", "coverage"),
     nthreads = 1
   )
 })
@@ -70,7 +77,8 @@ two_fun <- lapply(asplit(combn(supported_funcs, 2), 2), function(i) {
     biscuit_bedfiles,
     regions,
     funcs = i,
-    mval = FALSE,
+    c(4, 5),
+    c("cov", "beta"),
     nthreads = 1
   )
 })
@@ -80,17 +88,8 @@ one_reg <- lapply(regions, function(i) {
     biscuit_bedfiles,
     i,
     funcs = "all",
-    mval = FALSE,
-    nthreads = 1
-  )
-})
-
-mbeta <- lapply(c(T, F), function(i) {
-  run_test(
-    biscuit_bedfiles,
-    regions,
-    funcs = "all",
-    mval = i,
+    c(4, 5),
+    c("beta", "coverage"),
     nthreads = 1
   )
 })
@@ -100,18 +99,39 @@ threads <- lapply(c(1, 1), function(i) {
     biscuit_bedfiles,
     regions,
     funcs = "all",
-    mval = F,
+    c(4, 5),
+    c("beta", "coverage"),
     nthreads = i
   )
 })
+
+regions_df <- list(run_test(
+  biscuit_bedfiles,
+  regions.dt,
+  funcs = "all",
+  c(4, 5),
+  c("beta", "coverage"),
+  nthreads = 1
+))
+
+regions_gr <- list(run_test(
+  biscuit_bedfiles,
+  gr,
+  funcs = "all",
+  c(4, 5),
+  c("beta", "coverage"),
+  nthreads = 1
+))
+
 
 full_set <- list(
   one_file,
   one_fun,
   two_fun,
   one_reg,
-  mbeta,
-  threads
+  threads,
+  regions_df,
+  regions_gr
 )
 
 test_dims <- function(set) {
@@ -133,7 +153,7 @@ test_colnames <- function(set) {
 }
 
 test_that("dims", {
-  lapply(full_set, function(i) {
+  lapply(full_set[6], function(i) {
     test_dims(i)
   })
 })
@@ -147,121 +167,13 @@ test_that("colnames", {
 # error on bad 'fun' arguments
 test_that("bad fun", {
   expect_error(
-    summarize_meth_regions(biscuit_bedfiles, regions, fun = "none")
+    summarize_regions(biscuit_bedfiles, regions, fun = "none")
   )
   expect_error(
-    summarize_meth_regions(biscuit_bedfiles, regions, fun = c("all", "mean"))
+    summarize_regions(biscuit_bedfiles, regions, fun = c("all", "mean"))
   )
   expect_error(
-    summarize_meth_regions(biscuit_bedfiles, regions, fun = c("none", "mean"))
+    summarize_regions(biscuit_bedfiles, regions, fun = c("none", "mean"))
   )
 })
 
-m_sum <- read.csv(file.path(extdata, "summarize_regions_m_sum.test"))
-beta_sum <- read.csv(file.path(extdata, "summarize_regions_beta_sum.test"))
-m_mean <- read.csv(file.path(extdata, "summarize_regions_m_mean.test"))
-beta_mean <- read.csv(file.path(extdata, "summarize_regions_beta_mean.test"))
-beta_all <- read.csv(file.path(extdata, "summarize_regions_beta_all.test"))
-m_all <- read.csv(file.path(extdata, "summarize_regions_m_all.test"))
-
-test_that("summarize_meth_regions 1 thread sum", {
-  expect_equal(
-    m_sum,
-    summarize_meth_regions(biscuit_bedfiles, regions, fun = "sum", mval = TRUE, nthreads = 1)
-  )
-  expect_equal(
-    m_sum,
-    summarize_meth_regions(biscuit_bedfiles, gr, fun = "sum", mval = TRUE, nthreads = 1)
-  )
-  expect_equal(
-    m_sum,
-    summarize_meth_regions(biscuit_bedfiles, regions.dt, fun = "sum", mval = TRUE, feature_col = "names", nthreads = 1)
-  )
-  expect_equal(
-    beta_sum,
-    summarize_meth_regions(biscuit_bedfiles, regions, fun = "sum", mval = FALSE, nthreads = 1)
-  )
-})
-
-test_that("summarize_meth_regions 2 thread sum", {
-  expect_equal(
-    m_sum,
-    summarize_meth_regions(biscuit_bedfiles, regions, fun = "sum", mval = TRUE, nthreads = 2)
-  )
-  expect_equal(
-    beta_sum,
-    summarize_meth_regions(biscuit_bedfiles, regions, fun = "sum", mval = FALSE, nthreads = 2)
-  )
-})
-
-test_that("summarize_meth_regions 1 thread sum", {
-  expect_equal(
-    m_sum,
-    summarize_meth_regions(biscuit_bedfiles, regions, fun = "sum", mval = TRUE, nthreads = 1)
-  )
-  expect_equal(
-    beta_sum,
-    summarize_meth_regions(biscuit_bedfiles, regions, fun = "sum", mval = FALSE, nthreads = 1)
-  )
-})
-
-test_that("summarize_meth_regions 2 thread sum", {
-  expect_equal(
-    m_sum,
-    summarize_meth_regions(biscuit_bedfiles, regions, fun = "sum", mval = TRUE, nthreads = 2)
-  )
-  expect_equal(
-    beta_sum,
-    summarize_meth_regions(biscuit_bedfiles, regions, fun = "sum", mval = FALSE, nthreads = 2)
-  )
-})
-
-
-test_that("summarize_meth_regions 1 thread all", {
-  expect_equal(
-    m_all,
-    summarize_meth_regions(biscuit_bedfiles, regions, fun = "all", mval = TRUE, nthreads = 1)
-  )
-  expect_equal(
-    beta_all,
-    summarize_meth_regions(biscuit_bedfiles, regions, fun = "all", mval = FALSE, nthreads = 1)
-  )
-})
-
-test_that("summarize_meth_regions 2 thread all", {
-  expect_equal(
-    m_all,
-    summarize_meth_regions(biscuit_bedfiles, regions, fun = "all", mval = TRUE, nthreads = 2)
-  )
-  expect_equal(
-    beta_all,
-    summarize_meth_regions(biscuit_bedfiles, regions, fun = "all", mval = FALSE, nthreads = 2)
-  )
-})
-
-
-test_that("summarize_meth_regions 2 thread all bismark", {
-  expect_equal(
-    m_all,
-    summarize_meth_regions(bismark_bedfiles, regions, fun = "all", mval = TRUE, aligner = "bismark", nthreads = 2)
-  )
-  expect_equal(
-    m_all,
-    summarize_meth_regions(bismark_bedfiles, gr, fun = "all", mval = TRUE, aligner = "bismark", nthreads = 2)
-  )
-  expect_equal(
-    m_all,
-    summarize_meth_regions(bismark_bedfiles, regions.dt, fun = "all", mval = TRUE, aligner = "bismark", feature_col = "names", nthreads = 2)
-  )
-  expect_equal(
-    beta_all,
-    summarize_meth_regions(bismark_bedfiles, regions, fun = "all", mval = FALSE, aligner = "bismark", nthreads = 2)
-  )
-})
-
-test_that("summarize_meth_regions rownames", {
-  expect_equal(
-    rownames(summarize_meth_regions(biscuit_bedfiles, regions, set_region_rownames = T)),
-    rep(unname(regions), length(biscuit_bedfiles))
-  )
-})
