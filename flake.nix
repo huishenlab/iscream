0b27b6d4d2282eeadb0f1f5a8ee99f4afdd6b52c
James Eapen <james.eapen@vai.org>
2025-04-23 11:12:44 -0400

    chore(nix): switch to flake

---
+++ flake.nix
@@ -0,0 +1,106 @@
+{
+  description = "Flake to get iscream development environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      rPkgsLinkingTo = with pkgs.rPackages; [
+        Rcpp
+        RcppArmadillo
+        RcppProgress
+        RcppSpdlog
+        Rhtslib
+        stringfish
+      ];
+
+      rpkgsImports = with pkgs.rPackages; [
+        data_table
+        Matrix
+        parallelly
+      ];
+
+      rpkgsSuggests = with pkgs.rPackages; [
+        DelayedArray
+        GenomicRanges
+        ggplot2
+        ggridges
+        SummarizedExperiment
+      ];
+
+      rpkgsDevDeps = with pkgs.rPackages; [
+        covr
+        devtools
+        lobstr
+        pkgdown
+        rhub
+        roxygen2
+        styler
+        testthat
+        usethis
+        V8
+      ];
+
+      rpkgsBioc = with pkgs.rPackages; [
+        biscuiteer
+        bsseq
+      ];
+
+      pkgsDeps = with pkgs; [
+        R
+        htslib
+        pkg-config
+      ];
+
+      pkgsDevDeps = with pkgs; [
+        html-tidy
+        texlive.combined.scheme-full
+        checkbashisms
+        ccls
+      ];
+
+    in {
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = [
+          rPkgsLinkingTo
+          rpkgsImports
+          rpkgsSuggests
+          rpkgsDevDeps
+          pkgsDeps
+          pkgsDevDeps
+          # rpkgsBioc # takes a long time to install
+        ];
+        shellHook = ''
+          export I_R=${pkgs.R}/lib/R/include/
+          export I_RCPP=${pkgs.rPackages.Rcpp}/library/Rcpp/include/
+          export I_ARMA=${pkgs.rPackages.RcppArmadillo}/library/RcppArmadillo/include/
+          export I_PROGRESS=${pkgs.rPackages.RcppProgress}/library/RcppProgress/include/
+          export I_LOG=${pkgs.rPackages.RcppSpdlog}/library/RcppSpdlog/include/
+          export I_STRINGFISH=${pkgs.rPackages.stringfish}/library/stringfish/include/
+
+          export I_HTSLIB=${pkgs.htslib}/include/
+          export L_HTSLIB=${pkgs.htslib}/lib/libhts.a
+          export L_CURL=${pkgs.curl.out}/lib/libcurl.so
+
+          mkdir -p "$HOME/.R"
+          export R_LIBS_USER="$HOME/.R"
+
+          cat > .ccls << EOF
+          clang
+          %c -std=c11
+          %cpp -std=c++2a
+          -I$I_R
+          -I$I_RCPP
+          -I$I_ARMA
+          -I$I_HTSLIB
+          -I$I_CLOCK
+          -I$I_PROGRESS
+          -I$I_LOG
+          -I$I_STRINGFISH
+          EOF
+        '';
+       };
+    });
+}
